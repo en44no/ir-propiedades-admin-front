3da56d0054d44b945e36f897516ccbb275193517
unknown <Gregorio>
2022-02-23 22:33:34 -0300

    customer managment

---
+++ src/components/Customers/CreateCustomer.jsx
@@ -0,0 +1,132 @@
+import React, { useContext } from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormLabel,
+  Input,
+  Select,
+  Stack,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import CustomersContext from "../../context/Customers/CustomersContext";
+import { useForm } from "react-hook-form";
+import { MdLocationPin } from "react-icons/md";
+import { HiPlus } from "react-icons/hi";
+
+const CreateCustomer = (props) => {
+  const { addCustomer } = useContext(CustomersContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { full } = props;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const submitCustomer = (data) => {
+    addCustomer(data);
+    //document.getElementById("requiredCustomer").style.display = "none";
+    reset();
+    onClose();
+  };
+
+  return (
+    <>
+      <Button
+        w="8rem"
+        onClick={onOpen}
+        fontSize="15px"
+        leftIcon={<HiPlus fontSize="1.2rem" />}
+        mr={5}
+        mb={5}
+        borderRadius="9px"
+        variant="add-button"
+      >
+        Agregar
+      </Button>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent bg="defaultColor.400">
+          <DrawerCloseButton color="#fff" mt="2" />
+          <DrawerHeader color="#fff" borderBottomWidth="1px">
+            Agregar Cliente
+          </DrawerHeader>
+          <DrawerBody color="#fff">
+          <form id="CustomerForm" onSubmit={handleSubmit(submitCustomer)}>
+              <Stack spacing="14px">
+                <Box>
+                  <FormLabel htmlFor="CustomerName">Nombre</FormLabel>
+                  <Input
+                    {...register("name")}
+                    id="CustomerName"
+                    placeholder="Ingresa el nombre"
+                    autoComplete="off"
+                  />
+                </Box>
+                <Box>
+                  <FormLabel htmlFor="CustomerEmail">email</FormLabel>
+                  <Input
+                    {...register("name")}
+                    id="CustomerEmail"
+                    placeholder="Ingresa el email"
+                    autoComplete="off"
+                  />
+                </Box>
+                <Box>
+                  <FormLabel htmlFor="CustomerPhone">Telefono</FormLabel>
+                  <Input
+                    {...register("name")}
+                    id="CustomerPhone"
+                    placeholder="Ingresa el telefono"
+                    autoComplete="off"
+                  />
+                </Box>
+                <Box>
+                  <FormLabel htmlFor="customerDescription">
+                    Descripción
+                  </FormLabel>
+                  <Textarea
+                    {...register("description")}
+                    id="customerDescription"
+                    placeholder="Ingresa la descripción"
+                    autoComplete="off"
+                  />
+                </Box>
+              </Stack>
+            </form>
+          </DrawerBody>
+          <DrawerFooter borderTopWidth="1px">
+            <Button
+              color="#fff"
+              variant="cancel-action"
+              mr={3}
+              onClick={onClose}
+            >
+              Cancelar
+            </Button>
+            <Button
+              type="submit"
+              form="customerForm"
+              variant="confirm-add-button"
+            >
+              Confirmar
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default CreateCustomer;
